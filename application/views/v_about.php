ebcc1a485dbdc1172dcb50c6390c101f1f6ee5db
BarryuAhmadAji <barryuahmadaji0903@gmail.com>
2023-10-12 22:04:11 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,4 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex ut aliquam tenetur cumque ipsam impedit architecto, dolorum quisquam reprehenderit eum similique quos, minima sapiente.</p>
+</section>
---
+++ application/views/v_blog.php
@@ -0,0 +1,5 @@
+<section>
+    <p>Nama : Barryu ahmad aji </p>
+    <p>Kelas : 19.3A.04</p>
+    <p>Matkul : web programming II</p>
+</section>
